3db916f848a985f6c3347dbabf08a9cf52fc004f
tevinalvord <tevin.alvord@gmail.com>
2022-04-14 20:35:43 -0600

    site ready for some JS love

---
+++ Develop/script.js
@@ -0,0 +1,12 @@
+var currentDay = moment().format("dddd, MMMM Do");
+
+$("#currentDay").text(currentDay);
+
+var currentHour = moment().format("ha")
+
+switch(currentHour) {
+    case "9pm":
+
+        break;
+
+}
